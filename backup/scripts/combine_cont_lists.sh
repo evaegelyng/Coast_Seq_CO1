47a2e1afc6f34201d7e855095189fe74ddacd9c9
evaegelyng <eva.sigsgaard@bio.au.dk>
2022-10-19 11:22:38 +0200

    correct combine_cont_list.sh

--- backup/scripts/combine_cont_lists.sh
+++ backup/scripts/combine_cont_lists.sh
@@ -8,13 +8,11 @@
 head -n1 autumn/cont_list_sed_extr_autumn_2CVS4_.txt > autumn/autumn_cont_list.txt
     for fname in autumn/*.txt
     do
-        tail -n +2 $fname >> autumn/autumn_cont_list.txt
+        tail -n +2 $fname >> autumn/autumn_cont_list.tsv
     done
 
-#Something goes wrong in the spring list, so the file keeps growing
-
-#head -n1 spring/cont_list_sed_extr_spring_S6_.txt > spring/spring_cont_list.txt
-#    for fname in spring/*.txt
-#    do
-#        tail -n +2 $fname >> spring/spring_cont_list.txt
-#    done
+head -n1 spring/cont_list_sed_extr_spring_S6_.txt > spring/spring_cont_list.txt
+    for fname in spring/*.txt
+    do
+        tail -n +2 $fname >> spring/spring_cont_list.tsv
+    done
