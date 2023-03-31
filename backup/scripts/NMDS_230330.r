9b4601e9a78ff02985ca167b31d9ebc5be9ae173
evaegelyng <eva.sigsgaard@bio.au.dk>
2023-03-31 14:41:09 +0200

    update NMDS script

--- backup/scripts/NMDS_230330.r
+++ backup/scripts/NMDS_230330.r
@@ -17,9 +17,58 @@ otu_m<-data.frame(otu_table(COSQ_rare2),check.names=F)
 
 sam_dat<-data.frame(sample_data(COSQ_rare2), check.names=F)
 
+# Merge sample replicates, and keep no. of positive samples instead of read counts (see Antich et al 2022)
+## First add sample information
+sam_dat$sample<-sam_dat$root<-factor(substr(rownames(sam_dat),
+                          1,
+                          nchar(rownames(sam_dat))-1))
+asv_sam<-merge(otu_m,sam_dat[,c("sample","cluster")],by="row.names")
+## Remove Row.names
+asv_sam<-within(asv_sam,rm("Row.names"))
+
+## Remove samples from cluster 2, which was only sampled in spring
+asv_no_2<-asv_sam %>% filter(cluster!=2)
+
+## Aggregate replicate samples, counting positive samples per ASV
+asv_agg<-aggregate(. ~ sample, asv_no_2, function(x) sum(x > 0, na.rm = TRUE))
+
+## First, set row.names to sample
+row.names(asv_agg)<-asv_agg$sample
+## Remove sample and cluster column
+asv_agg=within(asv_agg,rm("sample","cluster"))
+
+#Reattach MOTU id for each ASV
+asv_t<-t(asv_agg)
+motu<-read.table("COSQ_final_ASV.tsv", sep="\t", header=T, check.names=F)
+row.names(motu)<-motu$id
+asv_motu<-merge(asv_t,motu[,c("motu","id")],by="row.names")
+row.names(asv_motu)<-asv_motu$Row.names
+## Remove ASV id column
+asv_motu=within(asv_motu,rm("id","Row.names"))
+
+## Count number of ASVs per MOTU
+asv_count<-asv_motu%>%count(motu)
+## Make a list of MOTUs with only one ASV
+one_asv<-asv_count[which(asv_count$n==1),]
+## Count how many MOTUs have only one ASV
+nrow(one_asv)
+## Remove MOTUs with only one ASV
+asv_final<-asv_motu[!asv_motu$motu %in% one_asv$motu,]
+## Remove motu column and transpose for use in NMDS
+asv_nmds<-within(asv_final,rm("motu"))
+asv_t<-t(asv_nmds)
+
+#Add taxonomy to ASV table and save to file
+classified<-read.table("NEW_pident97_data.txt",sep="\t", header=T, check.names=F)
+asv_final$phylum<-classified$phylum[match(asv_final$motu,classified$id)]
+asv_final$final.id<-classified$final.id[match(asv_final$motu,classified$id)]
+write.table(asv_final,"COSQ_ASV_table.tsv", sep="\t", quote=FALSE, row.names=TRUE)
+
+## The following part of the script needs to be updated, as the preceding part has changed
+
 # Merge samples from the same cluster, and keep no. of positive samples instead of read counts (see Antich et al 2022)
 ## First add cluster information (season is included to keep dataframe format for merging)
-asv_cluster<-merge(otu_m,sam_dat[,c("cluster","season")],by="row.names")
+asv_cluster<-merge(asv_t,sam_dat[,c("cluster","season")],by="row.names")
 ## Remove season and Row.names
 asv_cluster<-within(asv_cluster,rm("season","Row.names"))
 ## Aggregate samples from the same cluster, counting positive samples per ASV
@@ -33,31 +82,6 @@ asv_agg=within(asv_agg,rm("cluster"))
 ##Remove empty columns
 asv_agg<-asv_agg[, colSums(asv_agg != 0) > 0]
 
-#Reattach MOTU id for each ASV
-asv_agg_t<-t(asv_agg)
-motu<-read.table("COSQ_final_ASV.tsv", sep="\t", header=T, check.names=F)
-row.names(motu)<-motu$id
-asv_motu<-merge(asv_agg_t,motu[,c("motu","id")],by="row.names")
-row.names(asv_motu)<-asv_motu$Row.names
-## Remove cluster column
-asv_motu=within(asv_motu,rm("id","Row.names"))
-
-## Count number of ASVs per MOTU
-asv_count<-asv_motu%>%count(motu)
-## Make a list of MOTUs with only one ASV
-one_asv<-asv_count[which(asv_count$n==1),]
-## Count how many MOTUs have only one ASV
-nrow(one_asv)
-## Remove MOTUs with only one ASV
-asv_final<-asv_motu[!asv_motu$motu %in% one_asv$motu,]
-
-#Add taxonomy to ASV table and save to file
-classified<-read.table("NEW_pident97_data.txt",sep="\t", header=T, check.names=F)
-asv_final$phylum<-classified$phylum[match(asv_final$motu,classified$id)]
-asv_final$final.id<-classified$final.id[match(asv_final$motu,classified$id)]
-
-# Write table to a file
-write.table(asv_final,"ASV_table_clusters.tsv", sep="\t", quote=FALSE, row.names=TRUE)
 
 # Perform NMDS
 ## Remove MOTU id and transpose
